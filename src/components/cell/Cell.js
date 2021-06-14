6a43e5a2fd7945607d970bcdc009b068d0cdc33c
callapa1 <lopezcallapa@gmail.com>
2021-06-14 15:13:00 +0200

    added groups

--- src/components/cell/Cell.js
+++ src/components/cell/Cell.js
@@ -11,9 +11,35 @@ class Cell extends React.Component {
     const solution = this.props.solution
     for (let i=0 ; i < 81 ; i++) {
       if (solution[i] == " ") {
-        this.state.cells.push({id: i, value: ""})
+        this.state.cells.push({
+                                id: i,
+                                value: "",
+                                group: defineGroup(i)
+                              })
       } else {
-        this.state.cells.push({id: i, value: parseInt(solution[i])})
+        this.state.cells.push({
+                                id: i,
+                                value: parseInt(solution[i]), 
+                                original: true,
+                                group: defineGroup(i)
+                              })
+      }
+    }
+    function defineGroup(i) {
+      let groups = {1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[]}
+      for (let x=0, c=1; x < 62;) {
+        groups[c].push(x, x+1, x+2, x+9, x+10, x+11, x+18, x+19, x+20)
+        x += 3
+        c++
+        if (x == 9 || x == 36) {
+          x += 18
+        }
+      }
+      // Search in groups
+      for (let n=1;n<=9;n++) {
+        if (groups[n].includes(i)) {
+          return n
+        }
       }
     }
   }
@@ -22,10 +48,12 @@ class Cell extends React.Component {
     const id = event.target.id
     const number = parseInt(event.target.value)
     let new_cells = this.state.cells
-    new_cells[id] = {id: parseInt(id), value: number}
+    const group = new_cells[id].group
+    new_cells[id] = {id: parseInt(id), value: number, group:group}
     this.setState({
       cells: new_cells
     });
+    console.log(this.state.cells)
   }
   
   render() {
@@ -40,7 +68,7 @@ class Cell extends React.Component {
           y = 0
           x++
         }
-        if (data[i].value != "") {
+        if (data[i].value != "" && data[i].original) {
           return_array.push(<input
                               id={data[i].id}
                               className='filled_cell'
